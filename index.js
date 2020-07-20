0b792265594d262bad59800478333232084759b8
felipeGdM <felipegmelo.42@gmail.com>
2020-07-20 15:59:03 -0300

    :truck: Move simple_workflow.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { xmlConverter } = require('./src/xml_converter/xml_converter');
 
 // const json_sample = "ATV_ACTIVATED_CONTRACT.json";
 const json_sample = "simple_workflow.json";
-const dir = path.join(__dirname, "src/xml_converter/assets/");
+const dir = path.join(__dirname, "assets/");
 
 const workflow = JSON.parse(fs.readFileSync(path.join(dir, json_sample)));
 
