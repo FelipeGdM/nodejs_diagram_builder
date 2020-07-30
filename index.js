4bbba3f18e724645fd38d6f5a88ccf8cb15bec35
felipeGdM <felipegmelo.42@gmail.com>
2020-07-30 16:58:45 -0300

    :rocket: Declare export function

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+/* eslint-disable new-cap */
 const { xmlConverter } = require('./src/xml_converter/xml_converter');
 
-// const json_sample = "ATV_ACTIVATED_CONTRACT.json";
-const json_sample = "lanes_example.json";
-const dir = path.join(__dirname, "assets/");
+async function buildXmlDiagram(blueprintSpec, workflowName) {
+  const json2xml = new xmlConverter();
+  json2xml.build_graph(blueprintSpec, workflowName);
+  return json2xml.to_xml();
+}
 
-const workflow = JSON.parse(fs.readFileSync(path.join(dir, json_sample)));
-
-json2xml = new xmlConverter();
-
-json2xml.build_graph(workflow.blueprint_spec, workflow.name);
-
-json2xml.to_xml()
-    .then(console.log)
-    .catch(console.log);
+module.exports = buildXmlDiagram;
